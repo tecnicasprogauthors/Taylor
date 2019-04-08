e65e632df1e8726412aa8f46ca84eb5b660345bb
AlexRSchleder <48094141+AlexRSchleder@users.noreply.github.com>
2019-04-08 20:47:25 -0300

    Update Lambert.java

--- Taylor/taylor/src/main/java/tecnicas/mycompany/Lambert.java
+++ Taylor/taylor/src/main/java/tecnicas/mycompany/Lambert.java
@@ -1,4 +1,24 @@
-public class Lambert
+import java.lang.Math;
+import java.util.ArrayList;
+
+public static class Lambert
 {
-    
+    public static ArrayList<Double> lambertList(double x)
+    {
+        if (x >= 1/Math.E) throw (IllegalArgumentException);
+        else
+        {
+            ArrayList<Double> termos = new ArrayList<Double>();
+            double termo = 0;
+            for (int n = 1; termo >= Math.pow(10, -5); n++)
+            {
+                termo = ((Math.pow(-n, n-1))/(Util.fatorial(n)))*(Math.pow(x, n));
+            }
+        }
+        return sum;
+    }
+    public static double lambert(double x)
+    {
+        return util.somatorio(lambertList(x));
+    }
 }
