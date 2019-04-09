47c13413e1a2a511b419ce2bbb06cd3ffe3ea932
victoraso <48321884+victoraso@users.noreply.github.com>
2019-04-08 21:49:17 -0300

    Update Trig.java

--- Taylor/taylor/src/main/java/tecnicas/mycompany/Trig.java
+++ Taylor/taylor/src/main/java/tecnicas/mycompany/Trig.java
@@ -1,6 +1,37 @@
-public class Trig
-//victor
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Math;
 
-{
-    
-}
+public class Trig {
+
+    Util u = new Util();
+
+    public double cosine(double x, int n) {
+
+        List<Double> list = new ArrayList<Double>();
+
+        double resp = 0;
+
+        for (int i = 0; i < n; i++) {
+            resp = (Math.pow(-1, i) / u.fatorial(2 * i)) * Math.pow(x, (2 * i));
+            list.add(resp);
+            resp = x;
+        }
+
+        return u.somatorio(0, n, list);
+    }
+
+    public double sine(double x, int n) {
+
+        List<Double> list = new ArrayList<Double>();
+
+        double resp = 0;
+
+        for (int i = 0; i < n; i++) {
+            resp = (Math.pow(-1, i) / u.fatorial((2 * i) + 1)) * Math.pow(x, ((2 * i)) + 1);
+            list.add(resp);
+            resp = x;
+        }
+
+        return u.somatorio(0, n, list);
+    }
